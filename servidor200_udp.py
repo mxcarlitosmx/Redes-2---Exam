b44d08b254881891abe84a568e4a681ee743ec19
mxcarlitosmx <jhernandezp2200@alumno.ipn.mx>
2026-03-01 14:21:27 -0600

    Update UDP server configuration and timeout settings

--- servidor200_udp.py
+++ servidor200_udp.py
@@ -2,14 +2,17 @@ import socket
 
 # Configuracion
 IP = "0.0.0.0"
-PUERTO = 5006
-N_TOTAL = 200
+PUERTO = 9999
+N_TOTAL = 100
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((IP, PUERTO))
-sock.settimeout(7.0) # Tiempo de espera para detectar fin de rafaga
+
+# Aumentar tiempo de espera para que capte todo
+sock.settimeout(10.0) 
 
 print(f"[*] Servidor UDP esperando {N_TOTAL} paquetes...")
+print("[!] El calculo se hara automaticamente 10 segundos despues del ultimo paquete.")
 
 recibidos_unicos = set()
 
@@ -21,15 +24,12 @@ try:
         # Extraemos la informacion del encabezado
         partes = mensaje.split('|')
         seq = int(partes[0].split(':')[1])
-        indicador_fin = int(partes[2].split(':')[1])
         
         recibidos_unicos.add(seq)
         print(f"Recibido paquete #{seq}")
-        
-        if indicador_fin == 1: break # Indicador de fin
-
+    
 except socket.timeout:
-    print("\n[!] Tiempo agotado. Calculando metricas...")
+    print("\n[!] Tiempo agotado de espera. Calculando metricas finales...")
 
 # METRICAS DE LA PRACTICA
 n_recibidos = len(recibidos_unicos)
@@ -37,7 +37,7 @@ n_faltantes = [i for i in range(1, N_TOTAL + 1) if i not in recibidos_unicos]
 porcentaje_perdida = ((N_TOTAL - n_recibidos) / N_TOTAL) * 100
 
 print("\n" + "═"*40)
-print(f"   RESULTADOS DEL CONTROL ESTADISTICO")
+print(f"    RESULTADOS DEL CONTROL ESTADISTICO")
 print("═"*40)
 print(f"1. Paquetes esperados (N_total): {N_TOTAL}")
 print(f"2. Paquetes recibidos (N_unicos): {n_recibidos}")
